444a12a78b07d9693432bfd039ecc77911361d86
Jasper Van Proeyen <trianglejuice@mac.com>
2021-04-25 11:35:25 +0200

    Completed project

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const [filter, setFilter] = useState(2021);
@@ -19,6 +20,7 @@ function Expenses(props) {
     <div>
       <Card className="expenses">
         <ExpensesFilter year={filter} onFilterChanged={changeFilterHandler} />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
